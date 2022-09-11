08fb5e1262af509e02dc7b247e98f1d3c1048e4e
Noy Saadon <68056231+noysaadon@users.noreply.github.com>
2022-09-11 15:20:32 +0300

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import FavoriteList, Product
+from .models import FavoriteList, Product ,FavoriteandProduct
 
 admin.site.register(Product)
 admin.site.register(FavoriteList)
+admin.site.register(FavoriteandProduct)
